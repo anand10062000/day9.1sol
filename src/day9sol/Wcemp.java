c3160d6b392a7af5097c5879f376ea6b0eb24058
anand10062000 <anandkumarramisetti@gmail.com>
2021-09-22 11:01:07 +0530

    emp attendance

--- src/day9sol/Wcemp.java
+++ src/day9sol/Wcemp.java
@@ -1,10 +1,18 @@
 package day9sol;
 
+import java.util.Random;
+
 public class Wcemp {
 	public static void main(String[] args) {
 		
 	
 	System.out.println("welcome to employee");
+	Random ran=new Random();
+	int ran1=ran.nextInt(2);
+	if (ran1==1)
+		System.out.println("emp present");
+	else
+		System.out.println("emp absent");
 	}
 	
 
