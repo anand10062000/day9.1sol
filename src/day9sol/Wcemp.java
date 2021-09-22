84b56282d044be22b1034ec7cd110fb187423065
anand10062000 <anandkumarramisetti@gmail.com>
2021-09-22 11:13:39 +0530

    full day emp eage

--- src/day9sol/Wcemp.java
+++ src/day9sol/Wcemp.java
@@ -8,11 +8,19 @@ public class Wcemp {
 	
 	System.out.println("welcome to employee");
 	Random ran=new Random();
-	int ran1=ran.nextInt(2);
-	if (ran1==1)
-		System.out.println("emp present");
-	else
-		System.out.println("emp absent");
+	int ran1=ran.nextInt(3);
+	int wageperhr=20;
+	int fullday=8;
+	if (ran1==1) {
+		System.out.println("emp present full day");
+	    int empwage=fullday*wageperhr;
+	    System.out.println("empwage for full day emp is"+empwage);
+	    
+	}else if(ran1==2) {
+		System.out.println("emp present for parttime");
+	}else {
+		System.out.println("emp not present");}
+	
 	}
 	
 
